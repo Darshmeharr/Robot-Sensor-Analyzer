6561abe952bfa99f43e3d13d7fe0a25ce8610a51
D-Codes <99019785+Darshmeharr@users.noreply.github.com>
2026-05-27 15:27:12 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# Number of sensor readings
+
+NUM_READINGS = 20
+
+# Distance (cm)
+DISTANCE_MIN = 10
+DISTANCE_MAX = 100
+
+# Temperature (°C)
+TEMP_MIN = 20
+TEMP_MAX = 50
+
+# Battery (%)
+BATTERY_MIN = 30
+BATTERY_MAX = 100
+
+# Speed (km/h)
+SPEED_MIN = 1
+SPEED_MAX = 15
